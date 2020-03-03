9a503b8888914dc457cc0d67226118ecac292965
Andy Durette <duretteandy@gmail.com>
2020-03-03 14:08:44 -0500

    quelled lint warning for unused package

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {AppProvider} from './utils/AppContext';
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch/*, Route*/} from "react-router-dom";
 import "./scss/App.scss";
 
 function App() {
