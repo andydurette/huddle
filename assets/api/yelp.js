e579c8a237a5be1c5a618e22f03536bd7519bdac
WhitePeachTea <penglei2123@gmail.com>
2020-03-03 19:34:20 -0500

    api update

--- assets/api/yelp.js
+++ assets/api/yelp.js
@@ -9,27 +9,27 @@ class Yelp {
     constructor(){
     }
 
-    async searchRestoByLocation(location) {
+    async searchVenueByLocation(location) {
         let res = await axios.get(`https://api.yelp.com/v3/businesses/search?location=${location}`, config) 
         return res; 
     }
 
-    async searchRestoByCoord(lat, lon, radius) {
+    async searchVenueByCoord(lat, lon, radius) {
         let res = await axios.get(`https://api.yelp.com/v3/businesses/search?latitude=${lat}&longitude=${lon}&radius=${radius}`, config) 
         return res; 
     }
 
-    async searchRestoByCoordAndTerm(lat, lon, radius, term) {
+    async searchVenueByCoordAndTerm(lat, lon, radius, term) {
         let res = await axios.get(`https://api.yelp.com/v3/businesses/search?term=${term}&latitude=${lat}&longitude=${lon}&radius=${radius}`, config) 
         return res; 
     }
 
-    async getRestoDetail(businessId) {
+    async getVenueDetail(businessId) {
         let res = await axios.get(`https://api.yelp.com/v3/businesses/${businessId}`, config) 
         return res; 
     }
 
-    async searchRestoByLocationAndTerm(location, term) {
+    async searchVenueByLocationAndTerm(location, term) {
         let res = await axios.get(`https://api.yelp.com/v3/businesses/search?location=${location}&term=${term}`, config) 
         return res; 
     }
