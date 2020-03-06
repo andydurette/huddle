2b610307a7f403eec5044aeda36cb59b10529667
bernieip <bernie.ip414@gmail.com>
2020-03-05 23:18:14 -0500

    create Calendar and Main component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import Main from "./components/Main";
 
 function App() {
 	
@@ -15,6 +16,7 @@ function App() {
           <Switch>
            <Route path="/" exact activeClassName='is-active' component={Home}></Route>
            <Route path="/login" activeClassName='is-active' component={Login}></Route>
+           <Route path="/Main" activeClassName='is-active' component={Main}></Route>
           </Switch>
       </div>
       </Router>
