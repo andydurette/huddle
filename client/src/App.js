a4d02d46840a81fa3c7c1cd17bb528146f11f626
bernieip <bernie.ip414@gmail.com>
2020-03-07 13:42:49 -0500

    create adduser page

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Nav from "./components/Nav";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Main from "./components/Main";
-import AddUser from "./components/AddUser";
+import MainAddUser from "./components/MainAddUser";
 import SignUp from "./components/SignUp";
 import AuthTest from "./components/AuthTest";
 
@@ -20,7 +20,7 @@ function App() {
            <Route path="/" exact activeClassName='is-active' component={Home}></Route>
            <Route path="/login" activeClassName='is-active' component={Login}></Route>
            <Route path="/Main" activeClassName='is-active' component={Main}></Route>
-           <Route path="/adduser" activeClassName='is-active' component={AddUser}></Route>
+           <Route path="/adduser" activeClassName='is-active' component={MainAddUser}></Route>
            <Route path="/main" activeClassName='is-active' component={Main}></Route>
            <Route path="/signup" activeClassName='is-active' component={SignUp}></Route>
            <Route path="/authtest" activeClassName='is-active' component={AuthTest}></Route>
