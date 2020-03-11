330da54b896589a4c89af4d98d24d95adb133037
Brad <bkarulas@outlook.com>
2020-03-10 20:04:58 -0400

    team api routes

--- controller/routes/api-routes.js
+++ controller/routes/api-routes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const isAuthenticated = require("../isAuthenticated");
 const User = require("../../model/user"); 
 const passport = require("../../config/authConfigLocal");
-//const Team = require("../../model/team");
+const Team = require("../../model/team");
 const bcrypt = require("bcryptjs");
 const saltRounds = 10;
 
 let user = new User();
-//let team = new Team();
+let team = new Team();
 
 let apiRoutes = express.Router();
 
@@ -20,6 +20,8 @@ apiRoutes.post("/login", passport.authenticate("local", {failureMessage: "Incorr
 	res.send("Success");
 });
 
+
+//user routes
 apiRoutes.get("/user/team/:userid/:teamid", isAuthenticated, async (req, res) => {
 	let userId = req.params.userid;
 	let teamId = req.params.teamid;
@@ -40,7 +42,6 @@ apiRoutes.get("/user/info/:id", isAuthenticated,  async (req, res) => {
 	res.json(data);
 });
 
-
 apiRoutes.post("/user/signup/", isAuthenticated, async (req, res) => {
 	let firstName = req.body.firstName;
 	let lastName = req.body.lastName;
@@ -49,6 +50,46 @@ apiRoutes.post("/user/signup/", isAuthenticated, async (req, res) => {
 	let data = await user.createNew(firstName, lastName, email, password);
 	res.json(data);
 });
+
+
+//team routes
+apiRoutes.post("/team/new/", isAuthenticated, async (req, res) => {
+	let name = req.body.name;
+	let description = req.body.description;
+	let sport = req.body.sport;
+	let data = await team.createNew(name, description, sport);
+	res.json(data);
+});
+
+apiRoutes.post("/team/newmember/", isAuthenticated, async (req, res) => {
+	let teamId = req.body.team;
+	let userId = req.body.user;
+	let positionId = req.body.position;
+	let data = await team.addMember(teamId, userId, positionId);
+	res.json(data);
+});
+//*
+apiRoutes.put("/team/playerposition/:userId/:positionId", isAuthenticated, async (req, res) => {
+	let userId = req.params.userId;
+	let positionId = req.params.positionId;
+	let data = await team.updatePlayerPosition(userId, positionId);
+	res.json(data);
+});
+
+apiRoutes.delete("/team/deletemember/:teamId/:userId", isAuthenticated, async (req, res) => {
+	let teamId = req.params.teamid;
+	let userId = req.params.userid;
+	let data = await team.removeMember(teamId, userId);
+	res.json(data);
+});
+//*
+apiRoutes.delete("/team/delete/:teamId", isAuthenticated, async (req, res) => {
+	let teamId = req.params.teamid;
+	let data = await team.deleteTeam(teamId);
+	res.json(data);
+});
+
+
 
 
 module.exports = apiRoutes;
