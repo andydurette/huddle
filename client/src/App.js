d496a3fea5a9fe6ca07af6c0d2856c902cb53f11
Gavin <ekkambyte@gmail.com>
2020-03-12 21:15:05 -0400

    added stuff

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,11 @@ import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import Main from "./components/Main";
 import SignUp from "./components/SignUp";
+import AuthTest from "./components/AuthTest";
+import Teamviewer from "./components/TeamViewer";
+import TeamMaker from "./components/TeamMaker";
 
 function App() {
 	
@@ -16,7 +20,11 @@ function App() {
           <Switch>
            <Route path="/" exact activeClassName='is-active' component={Home}></Route>
            <Route path="/login" activeClassName='is-active' component={Login}></Route>
+           <Route path="/main" activeClassName='is-active' component={Main}></Route>
            <Route path="/signup" activeClassName='is-active' component={SignUp}></Route>
+           <Route path="/authtest" activeClassName='is-active' component={AuthTest}></Route>
+           <Route path="/teamview" activeClassName='is-active' component={Teamviewer}></Route>
+           <Route path="/teammake" activeClassName='is-active' component={TeamMaker}></Route>
           </Switch>
       </div>
       </Router>
---
+++ client/src/components/MainAddUser.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import Footer from "./Footer";
+import AddUser from "./AddUsers/AddUser";
+import User from "./AddUsers/User";
+
+function MainAddUser() {
+
+    const [users, setUsers] = useState([]);
+
+    function createUser(newUser) {
+        setUsers(prevUsers => {
+            return [...prevUsers, newUser];
+        });
+    }
+
+    function deleteUsers(id) {
+        setUsers(prevUsers => {
+            return prevUsers.filter((userItem, index) => {
+                return index !== id;
+            });
+        });
+    }
+
+    return (
+        <section id="wrapper" className="user">
+            <div id="wrapper-contents">
+                <AddUser onAdd={createUser}/>
+                {users.map((userItem, index) => {
+                    return (
+                        <User
+                            key={index}
+                            id={index}
+                            name={userItem.name}
+                            onDelete={deleteUsers}
+                        />
+                    );
+                })}
+            </div>
+            <Footer/>
+        </section>
+    )
+}
+
+export default MainAddUser;
