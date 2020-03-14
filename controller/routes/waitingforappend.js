2cf405a7555f8191fb356e38b17fb88a152f5884
WhitePeachTea <penglei2123@gmail.com>
2020-03-14 13:47:45 -0400

    update route

--- controller/routes/waitingforappend.js
+++ controller/routes/waitingforappend.js
@@ -3,11 +3,34 @@ const isAuthenticated = require("../isAuthenticated");
 const passport = require("../../config/authConfigLocal");
 const yelp = require("../../api/yelp");
 const weather = require("../../api/weather");
-const Event = require("../model/classes/event");
-let event = new Event();
+const event = require("../model/classes/event");
+const venue = require("../model/classes/venue");
 
 
-apiRoutes.post("/api/search/venues", isAuthenticated, async function(req, res){
+let event = new event();
+let venue = new venue();
+
+let apiRoutes = express.Router();
+
+
+const callApi = async () => {
+    try {
+      const token = await getTokenSilently();
+      console.log('token: ', token);
+      const response = await fetch("https://dev-0ec6it0o.auth0.com/userinfo", {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      const responseData = await response.json();
+      setShowResult(true);
+      setApiMessage(responseData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+apiRoutes.post("/api/venues/search", isAuthenticated, async function(req, res){
 	let location = req.body.location;
 	let term = req.body.term;
 	let response;
@@ -23,17 +46,46 @@ apiRoutes.post("/api/search/venues", isAuthenticated, async function(req, res){
 	res.json(results);
 });
 
-
-apiRoutes.get("/api/venues/:id", isAuthenticated, async function(req, res){
+apiRoutes.get("/api/venues/:businessId", isAuthenticated, async function(req, res){
 	let businessId = req.params.id;
 	let response = await yelp.getVenueDetail(businessId);
 	res.json(response.data);
 });
 
-apiRoutes.get("/api/venues/:id", isAuthenticated, async function(req, res){
+apiRoutes.post('/api/event/venue/add', isAuthenticated, async function(req, res){
 	let businessId = req.params.id;
-	let response = await yelp.getVenueDetail(businessId);
-	res.json(response.data);
+	response =  await yelp.getVenueDetail(businessId);
+	let name = req.body.name.replace("'","''");
+    let yelpId = req.body.id;
+    let lon = req.body.coordinates.longitude;
+    let lat = req.body.coordinates.latitude;
+    let city = req.body.location.city;
+    let address = JSON.stringify(req.body.location);
+	let website = req.body.url;
+	let phone = req.body.phone;
+    let results = await venue.addNew(name, yelpId, lon, lat, city, address, website, phone);
+	console.log('Adding a venue to the databse');
+	res.json(results);
+});
+
+
+apiRoutes.post("/api/event/venue/add", isAuthenticated, async function(req, res){
+	let businessId = req.param.id;
+	response = await venue.getDetails(businessId);
+	let eventId = req.param.event_id;
+	let venueId = res.venue_id
+	result = await event.updateEvent(eventId, venueId);
+	res.json(result);
+});
+
+
+apiRoutes.delete("api/event/:venuesId", isAuthenticated, async (req, res) => {
+	let eventId = req.param.event_id;
+	let id = req.param.venue_id
+	let venueId = null;
+	response = await event.updateEvent(eventId, venueId);
+	result = await venue.delete(id);
+	res.json(data);
 });
 
 apiRoutes.get("/api/weather", isAuthenticated, async function(req, res){
@@ -43,9 +95,9 @@ apiRoutes.get("/api/weather", isAuthenticated, async function(req, res){
 	for (i in breakPoint){
 	let response = await weather.weatherForcastbyLocation(location);
 	if (moment(response.dt_txt[i]).format('l') === moment(event.getDate(eventId)).format('l')) {
-		var temp = Math.round(response.list[breakPoint[i]].main.temp - 273.15 ) + " " + String.fromCharCode(176) + "C";
-		var humidity = response.list[breakPoint[i]].main.humidity + "%";
-		var icon = response.list[breakPoint[i]].weather[0].icon; 
+		var temp = Math.round(response.list[i].main.temp - 273.15 ) + " " + String.fromCharCode(176) + "C";
+		var humidity = response.list[i].main.humidity + "%";
+		var icon = response.list[i].weather[0].icon; 
 		let results =[temp, humidity, icon];
 		res.json(results);
 	}
@@ -54,4 +106,6 @@ apiRoutes.get("/api/weather", isAuthenticated, async function(req, res){
 	}
 	}
 });
-*/
+
+
+module.exports = apiRoutes; */
